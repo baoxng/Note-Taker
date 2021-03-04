2e2d4050ab625019ae9ca2dca04db3dc026f2299
Bao Xiong <bao.xiong2011@gmail.com>
2021-03-04 16:36:38 -0600

    Set up the express app as well as the data parsing.

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const express = require("express");
 const path = require("path");
 const fs = require ("fs");
 
+//Setting up Express APP
+const app= express ();
+const PORT= pross.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Data
+
+//Routes
+
