9f6c5b09005a74f96b2686ac5078d0a891868fc4
Bao Xiong <bao.xiong2011@gmail.com>
2021-03-04 22:25:47 -0600

    Created the routes and write file function.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require ("fs");
 
 //Setting up Express APP
-const app= express ();
+const app= express();
 const PORT= pross.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
@@ -15,3 +15,23 @@ app.use(express.json());
 
 //Routes
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.post('/api/notes', (req, res)=> {
+    fs.readFile(__dirname + '/db/db.json', 'utf8', (err, notes)=> {
+        if (err) throw err;
+    })
+    notes= JSON.parse(notes)
+    const id = notes[notes.length - 1].id + 1
+    const newNote = { title: req.body.title, text: req.body.text, id: id }
+    const currentNote = notes.concat(newNote)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(currentNote), (err,data)=> {
+        if (err) throw err;
+        console.log(currentNote);
+        res.json(currentNote)    
+    })
+
+});
