526f0293acc6276ed62f5d28682b74bc62d6ab2c
Bao Xiong <bao.xiong2011@gmail.com>
2021-04-05 15:10:07 -0500

    Troubleshooting note taker.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const path= require("path");
 const fs= require("fs");
+const { json } = require("express");
 
 
 //Setting up Express APP
@@ -12,6 +13,8 @@ const homepage= path.join(__dirname, "/public");
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static('public'));
 
 //routes
 app.get ("/notes", (req, res) => {
@@ -32,21 +35,37 @@ app.get("*", (req, res)=>{
 });
 
 //creating post on page
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res)=>{
+  let savedNotes= JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNotes= req.body;
+  let noteID= (savedNotes.length).toString();
+  newNotes.id= noteID;
+  savedNotes.push(newNotes);
 
-  let newNote = req.body;
-  let uniqueId = (data.length).toString();
-  console.log(uniqueId);
-  newNote.id = uniqueId;
-  data.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  console.log("Notes have been saved to db.json file. Content:", newNotes)
+  res.json(savedNotes);
+})
+
+
+//deleting post on page
+app.delete("/api/notes/:id", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+  let newID = 0;
+  console.log(`Deleting note with ID ${noteID}`);
+  savedNotes = savedNotes.filter(currNote => {
+      return currNote.id != noteID;
+  })
   
-  fs.writeFileSync("./db/db.json", JSON.stringify(data),(err) => {
-      if (err) throw (err);        
-  }); 
+  for (currNote of savedNotes) {
+      currNote.id = newID.toString();
+      newID++;
+  }
 
-  res.json(data);    
-
-});
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  res.json(savedNotes);
+})
 
 //Listener
 app.listen(PORT, function () {
